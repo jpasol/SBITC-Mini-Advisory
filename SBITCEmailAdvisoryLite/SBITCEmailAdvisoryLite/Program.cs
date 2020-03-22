117b97460eec6c0a34b2777afdf6d36a564f5650
Jaspher Jan Pasol <jpasol@sbitc.ph>
2020-03-23 07:49:25 +0800

    Alpha v1.0.0
    - Testing API

---
+++ SBITCEmailAdvisoryLite/SBITCEmailAdvisoryLite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Mail;
+using System.Configuration;
+using System.Collections.Specialized;
+
+namespace SBITCEmailAdvisoryLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExeConfigurationFileMap configMap = new ExeConfigurationFileMap();
+            configMap.ExeConfigFilename = @$"{Environment.CurrentDirectory}\App.config";
+            Configuration config = ConfigurationManager.OpenMappedExeConfiguration(configMap, ConfigurationUserLevel.None);
+
+            var smtpSettings = config.SectionGroups["AdvisorySettings"].Sections["SMTP"];
+            var messageSettings = config.SectionGroups["AdvisorySettings"].Sections["Message"];
+
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient();
+
+                mail.From = new MailAddress("your_email_address@gmail.com");
+                mail.To.Add("to_address");
+                mail.Subject = "Test Mail";
+                mail.Body = "This is for testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("username", "password");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+    }
+}
